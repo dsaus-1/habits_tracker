000198219a32eb3cf91638c8338d9072e54dbd8b
dsaus <saushkin.reg@gmail.com>
2023-04-13 18:47:28 +0300

    обновления докер конфигурации (финальная версия)

--- config/settings.py
+++ config/settings.py
@@ -132,12 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-        BASE_DIR / 'static/',
-)
+STATIC_URL = '/static/'
 
-STATIC_ROOT = 'static_files/'
+STATIC_ROOT = 'static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
